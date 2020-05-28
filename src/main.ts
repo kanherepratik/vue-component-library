d0d5baeaccfc48ae89a01f17eb73f5e4b749ea7b
prateek kanhere <kanherepratik@gmail.com>
2020-05-28 12:17:23 +0530

    add component and entry point

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,13 @@
-import Vue, { VNode } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import storeHelper from './store/plugins/storeHelper';
+import { IValidation, IValidationRule } from '@/shared/interfaces';
+import { VALIDATORS } from '@/shared/enum';
+import * as components from './components';
 
-Vue.config.productionTip = false;
-Vue.use(storeHelper);
+const install = (Vue: any, options: any): void => {
+  for (const component in components) {
+    if ((components as any)[component]) {
+      Vue.component((components as any)[component].name, (components as any)[component]);
+    }
+  }
+};
 
-// if (SENTRY_ENV !== 'development') {
-//     import(/* webpackChunkName: "sentry" */ '@sentry/browser').then((Sentry) => {
-//         Sentry.init({
-//             dsn: 'https://d850ef5acf4f4addab2477c9523e6584@app.getsentry.com/1342484',
-//             integrations: [new Sentry.Integrations.Vue({ Vue })],
-//             environment: SENTRY_ENV
-//         });
-//     });
-// }
-
-// Vue.prototype.STATIC_URL = STATIC_URL;
-
-new Vue({
-  router,
-  store,
-  render: (h): VNode => h(App),
-}).$mount('#app');
+export default { install, VALIDATORS };
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,8 @@
-export const IS_MOBILE_DEVICE: boolean = (window as any).IS_MOBILE || false;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+import { ISuffixDivisorMap } from './interfaces';
+
+export const SUFFIX_DIVISOR_MAP: ISuffixDivisorMap = {
+  thousand: 1000,
+  lakh: 100000,
+  crore: 10000000,
+};
